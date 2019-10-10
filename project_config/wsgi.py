7fe6a2935d965e00718de4b41894a70d52478d4c
Mfon Eti-mfon <mfonetimfon@gmail.com>
2019-10-10 02:12:20 +0100

    Clean up project config folder.

--- project_config/wsgi.py
+++ project_config/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for paranoia project.
+WSGI config for project_config project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'paranoia.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_config.settings')
 
 application = get_wsgi_application()

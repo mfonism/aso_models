379a3eb1f2755d6c4f6f6e5dd6b0b3649e193ad4
Mfon Eti-mfon <mfonetimfon@gmail.com>
2019-10-18 15:45:12 +0100

    Add doc strings to manager classes.

--- aso_models/managers.py
+++ aso_models/managers.py
@@ -4,10 +4,27 @@ from .querysets import ShrewdQuerySet, NaiveQuerySet, RecycleBinQuerySet
 
 
 class ShrewdManager(BaseManager.from_queryset(ShrewdQuerySet)):
+    '''
+    Shrewd manager for models.
+
+    Manages only objects of the model which are __not in__ the recycle bin.
+    '''
     pass
+
 
 class NaiveManager(BaseManager.from_queryset(NaiveQuerySet)):
+    '''
+    Naive manager for models.
+
+    Manages __all__ the objects of the model in the database.
+    '''
     pass
 
+
 class RecycleBinManager(BaseManager.from_queryset(RecycleBinQuerySet)):
+    '''
+    Recycle bin manager for models.
+
+    Manages only objects of the model which are __in__ the recycle bin.
+    '''
     pass

65dbfd98cf6b05b5560b0de20a52ca33525c7718
Mfon Eti-mfon <mfonetimfon@gmail.com>
2019-10-18 17:27:34 +0100

    Add doc strings to queryset classes.

--- aso_models/querysets.py
+++ aso_models/querysets.py
@@ -4,14 +4,23 @@ from django.utils import timezone
 
 
 class ShrewdQuerySet(models.QuerySet):
+    '''
+    Shrewd queryset for models.
+
+    Fetches only objects of the underlying model which
+    are __not__ in the recycle bin.
+    '''
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # query for only active, non soft-deleted objects
+        # ensure it only ever fetches objects which are not in the recycle bin
         self.query.add_q(
             Q(deleted_at__isnull=True, activated_at__isnull=False)
         )
 
     def delete(self):
+        '''
+        Send the fetched objects into the recycle bin.
+        '''
         return self.update(deleted_at=timezone.now(), activated_at=None), {}
 
     def restore(self):
@@ -22,6 +31,11 @@ class ShrewdQuerySet(models.QuerySet):
 
 
 class NaiveQuerySet(models.QuerySet):
+    '''
+    Naive queryset for models.
+
+    Fetches __every__ object of the underlying model available on the database.
+    '''
     def restore(self):
         raise AssertionError(
             'Restore operation is not allowed on a naive queryset.\n'
@@ -30,12 +44,21 @@ class NaiveQuerySet(models.QuerySet):
 
 
 class RecycleBinQuerySet(models.QuerySet):
+    '''
+    Recycle bin queryset for models.
+
+    Fetches only objects of the underlying model which
+    are __in__ the recycle bin.
+    '''
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # query for only soft-deleted objects
+        # ensure it only ever fetches objects that are in the recycle bin
         self.query.add_q(
             Q(deleted_at__isnull=False)
         )
 
     def restore(self):
+        '''
+        Send the fetched objects out of the recycle bin.
+        '''
         return self.update(deleted_at=None, activated_at=timezone.now())
